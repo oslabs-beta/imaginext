3f501b75b97cbb7740c726906bf9856c1c017d2d
MoMo <techsammie666@gmail.com>
2022-11-26 16:32:22 -0500

    added submit functionality; fix the mismatch of hydration issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Tree from 'react-d3-tree'
-import {useState,useRef, useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { inputData, child } from '../public/types'
 
 /**
  * react-3d-tree is looking for the data entry in the following format: 
@@ -15,8 +16,9 @@ import {useState,useRef, useEffect} from 'react'
 }
  */
 
+
 export default function Home() {
-  const test = {
+  const test : inputData = {
     name: 'Pages',
     children: [
       {
@@ -24,34 +26,36 @@ export default function Home() {
         attributes: {
             path: "pages/_app.tsx",
         },
-          children:[]
+        children: undefined
       },
        {
         name: "index.tsx",
         attributes: {
             path: "pages/index.tsx",
         },
-          children:[]
+        children: undefined
       },
            {
         name: "api",
         attributes: {
             path: "pages/api",
         },
-             children: [
-               {
-                 name: "hello.ts",
-                 attributes: {
-                   path:"pages/api/hello.ts"
-                 },
-               }
-          ]
+        children: [
+          {
+            name: "hello.ts",
+            attributes: {
+              path:"pages/api/hello.ts"
+            },
+            children: undefined
+          }
+        ]
       },
     ]
   }
   const shouldRecenterTreeRef = useRef(true);
   const [treeTranslate, setTreeTranslate] = useState({ x: 0, y: 0 });
   const treeContainerRef = useRef(null);
+  const [treeData, setTreeData] = useState(<h6>Please Upload A Project</h6>)
 
   useEffect(() => {
     if (treeContainerRef.current && shouldRecenterTreeRef.current) {
@@ -65,21 +69,54 @@ export default function Home() {
     }
   });
 
-  return (
-    // <div id='treeWrapper' style={{ width: '50em', height: '20em' }}>
-    //   <Tree data = {test} />
-    //   </div>
-        <div ref={treeContainerRef} style={{ height: '100vh' }}>
+  const getDynamicPathClass = ({ source, target }, orientation) => {
+    if (!target.children) {
+      // Target node has no children -> this link leads to a leaf node.
+      return 'link__to-leaf';
+    }
+
+    // Style it as a link connecting two branch nodes by default.
+    return 'link__to-branch';
+  };
+   
+  const handleSubmit = () => {
+    const input = document.getElementById("submitInput");
+    let value: string = input.value;
+    if(value === null) value = "";
+    console.log(value);
+
+    // fetch('/data', {
+    //   method: 'POST',
+    //   header: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body:JSON.stringify(value),
+    // })
+
+    setTreeData(
       <Tree
         data={test}
         collapsible={true}
-        pathFunc="step"
+        pathFunc="diagonal"
         translate={treeTranslate}
         orientation="vertical"
         rootNodeClassName="node__root"
         branchNodeClassName="node__branch"
         leafNodeClassName="node__leaf"
+        pathClassFunc={getDynamicPathClass}
       />
+    );
+  }
+
+
+  return (
+    
+    <div ref={treeContainerRef} style={{ height: '100vh' }}>
+
+      <input id="submitInput"></input>
+      <button onClick={handleSubmit}>Submit</button>
+
+      {treeData}
     </div>
   )
 }
