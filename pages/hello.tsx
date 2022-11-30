b6ebeaccdafae5d38774969bfc97956a13dd2396
Richter Lau <ar.rick@hotmail.com>
2022-11-29 21:52:00 -0500

    Richter/update hello.tsx (#10)
    * richter/update parse
    
    * richter/setData from endpoint

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react'
+
 export default function Hello() {
+    const [data, setData] = useState(null)
+
+    const testClick = () => {
+        fetch('http://localhost:3000/api/hello')
+          .then((response) => response.json())
+          .then((json) => {
+            setData(json)
+          })
+    
+        //   const res = await fetch(`http://localhost:3000/api/hello`)
+        //   const data = await res.json()
+        // console.log(data)
+      }
+
+      console.log(data)
+
     return (
-        <h1>Demo!</h1>
+        <div>
+            <button onClick={testClick}>Test</button>
+        </div>
     )
 }
-
-// test
