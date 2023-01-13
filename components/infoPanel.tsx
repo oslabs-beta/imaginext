82b8c5d0ba9b180de6679c4e94034eaf1a36b7ee
hollynigh <101919978+hollynigh@users.noreply.github.com>
2023-01-12 18:24:35 -0800

    Display endpoints (#26)
    * merge dev
    
    * display endpoint on frontend

--- components/infoPanel.tsx
+++ components/infoPanel.tsx
@@ -1,16 +1,17 @@
 import {attribute, attributes} from '../public/types'
 
 export default function InfoPanel (props: {att:attribute}) {
-    let path: string = props.att.path;
+  let path: string = props.att.path;
   const dataRenderMethod: string = props.att.dataRenderMethod;
-   const clearPath = (path: string):string => {
+  const endpoint: string = props.att.fetchURL !== undefined ? props.att.fetchURL : '';
+  const clearPath = (path: string):string => {
     let newPath = path.split('/')
-     for (let i = 0; i < newPath.length; i++){
-         if (newPath[i] === 'pages') newPath = newPath.slice(i);
-     }
-     const newPathString = newPath.join('/')
-     return newPathString;
-   }
+    for (let i = 0; i < newPath.length; i++){
+        if (newPath[i] === 'pages') newPath = newPath.slice(i);
+    }
+    const newPathString = newPath.join('/')
+    return newPathString;
+  }
   path = clearPath(path);
 
   return (
@@ -18,6 +19,7 @@ export default function InfoPanel (props: {att:attribute}) {
       <div className='card-content'>
         <div>Path: {path}</div>
         <div>Data Render Method: {dataRenderMethod}</div>
+        <div>Fetch Endpoint: {endpoint}</div>
         <div>Props:</div>
       </div>
     </>
