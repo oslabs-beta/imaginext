c22f8cc6f9a38097a076aff0367d7d736fb4f54d
MoMo <techsammie666@gmail.com>
2023-01-14 14:44:59 -0500

    modified styles to fit in all incoming data

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+export default function About({ message }) {
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+export function getServerSideProps() {
+  return {
+      props: {
+          message: 'Welcome to the About Page',
+          testProp: 'nothing to see here'},
+  };
+}
