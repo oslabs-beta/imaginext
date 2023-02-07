78a2a40ba9afce38f8f4274942689d5e3525d1b2
hollynigh <101919978+hollynigh@users.noreply.github.com>
2023-02-06 21:50:14 -0800

    Parse current dir (#31)
    * updating to allow localhost to parse current project by default on initial launch. only works if pages directory is not nested
    
    * adding .npmignore
    
    * adding custom tsconfig file to avoid next.js overwriting it

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -7,13 +7,16 @@ import runParser from '../../lib/parser';
 
 // todo: add support to input a link or drag and drop.
 // process.cwd -> goes to the root of this project
-const currentProjectPath = path.join(process.cwd(), 'pages');
+// TODO: currently this only works assuming pages directory is not nested. need to refactor to account for nested directories
+const currentProjectPath = path.resolve(process.cwd() + '/../../pages');
 // const otherPath = '/Users/richter/Downloads/playground/my-app/pages'; // put in some other path to check
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data> // setting the type from line 8
-) {
+  ) {
+    
+  console.log('currentProjectPath: ', currentProjectPath)
 
   function traverseDir(dir:string) {
     const validFileType:Array<string> = ['.ts', '.tsx', '.js', 'jsx'];
@@ -53,17 +56,38 @@ export default async function handler(
     // console.log('parsed node attributes being sent to frontend',arr)
     return arr
   }
-  if (req.method === 'POST') {
-    const path = req.body.path.replace(/\\/g, '/');
 
+  // function findPagesDir(dir:string) {
+  //   fs.access(dir, function(error) {
+  //     if (error) {
+  //       console.log('pages directory ')
+  //     }
+  //   })
+  // }
+
+  if (req.method === 'POST') {
+    let path;
+    if (req.body.path === 'undefined') {
+      path = currentProjectPath;
+    } else {
+      path = req.body.path.replace(/\\/g, '/');
+    }
     const lastPathItem = path.substring(path.lastIndexOf('/') + 1);
 
     // if input path is it's root. get into the pages directory and travese it instead.
     if (lastPathItem.toLowerCase() !== 'pages') {
-      res.status(200).json({ name: 'Pages', children: traverseDir(path + '/pages')})
+      if (fs.existsSync(path)) {
+        res.status(200).json({ name: 'Pages', children: traverseDir(path + '/pages')})
+      }
+      // TODO: add functionality if pages directory is nested. currently just sending an error.
+      else {
+        res.status(400).send({ name: 'error: invalid data', children: undefined});
+      }
+
     } else {
       res.status(200).json({ name: 'Pages', children: traverseDir(path)})
     }
+    
   } else {
     // change the argument in traverseDir
     res.status(200).json({ name: 'Pages', children: traverseDir(currentProjectPath)})
