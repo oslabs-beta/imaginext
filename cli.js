0329bac64a1655a9d47b1c4e66100df9421b29ea
hollynigh <hollynigh@yahoo.com>
2023-02-08 19:27:27 -0800

    updating cli.js

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const exec = require('child_process').exec;
 
-console.log("Launching imagiNEXT on port 3333...To view, go to http://localhost/3333. To stop server, use control/command C");
+console.log("Launching imagiNEXT on port 3333...To view, go to http://localhost:3333. To stop server, use CTRL + C");
 
 exec('PORT=3333 npm --prefix ./node_modules/imaginext run dev', (err) => {
   if (err) {
